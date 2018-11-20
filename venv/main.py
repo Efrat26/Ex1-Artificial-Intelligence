554f7d0dc72d2e9e8d0d12639cbdbf8581a81085
efrat <efisofer26@gmail.com>
2018-11-20 15:14:01 +0200

    first commit

---
+++ venv/main.py
@@ -0,0 +1,11 @@
+#Efrat Sofer 304855125
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print 'the program needs one input argument'
+        return
+    input_file = sys.argv[1]
+
+if __name__ == "__main__":
+    main()
