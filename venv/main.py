de643f3ce4c8af6c98556bdecedaf71b2f469cf1
efiso <efisofer26@gmail.com>
2018-11-22 01:00:51 +0200

    added BFS algorithm (still not checked). added a conversion to path moves on the board from the states visited.

--- venv/main.py
+++ venv/main.py
@@ -1,4 +1,5 @@
 import sys
+import collections
 #Efrat Sofer 304855125
 
 def getGoalState(board_size):
@@ -36,12 +37,12 @@ def getNeighbors(position, board_size):
     # top neighbor
     if zero_index - board_size > 0:
         neighbors.insert(len(neighbors), splitted_position[zero_index - board_size])
-    # right neighbor
-    if zero_index < board_size*board_size - 1:
-        neighbors.insert(len(neighbors), splitted_position[zero_index + 1])
     # bottom neighbor
     if zero_index + board_size < board_size*board_size - 1:
         neighbors.insert(len(neighbors), splitted_position[zero_index + board_size])
+    # right neighbor
+    if zero_index < board_size * board_size - 1:
+        neighbors.insert(len(neighbors), splitted_position[zero_index + 1])
     # left neighbor
     if zero_index > 0:
         neighbors.insert(len(neighbors), splitted_position[zero_index - 1])
@@ -49,28 +50,59 @@ def getNeighbors(position, board_size):
     return createPositionsFromNeighbors(neighbors, position)
 
 
+def getPath(solution_list):
+    if len(solution_list) < 2:
+        return
+    path = []
+    for i in range(1, len(solution_list)):
+        next = solution_list[i].split('-')
+        prev = solution_list[i-1].split('-')
+        zero_ind_next = next.index('0')
+        zero_ind_prev = prev.index('0')
+        if zero_ind_next - zero_ind_prev == 1:
+            path.append('L')
+        elif zero_ind_next - zero_ind_prev == -1:
+            path.append('R')
+        elif zero_ind_next - zero_ind_prev == 3:
+            path.append('U')
+        elif zero_ind_next - zero_ind_prev == -3:
+            path.append('D')
+    return path
 
-def DLS(src, target, limit, board_size):
+def DLS(src, target, limit, board_size, solution_list):
     if src == target:
         return True
     if limit <= 0:
         return False
     neighbors = getNeighbors(src, board_size)
     for n in neighbors:
-        if DLS(n, target, limit - 1, board_size):
+        if DLS(n, target, limit - 1, board_size, solution_list):
+            solution_list.append(n)
             return True
     return False
 
 
 def IDDFS(src, target, max_depth, board_size):
     for limit in range(0, max_depth):
+        solution = [src]
         print 'begin depth: ' + str(limit)
-        if DLS(src, target, limit, board_size) == True:
-            return True
-    return False
+        if DLS(src, target, limit, board_size, solution) == True:
+            path = getPath(solution)
+            return path
+    return []
 
-def BFS(board):
-    print 'BFS'
+def BFS(board_size, root, goal):
+    queue = collections.deque([root])
+    number_of_verteces_checked = 0
+    while queue:
+        vertex = queue.popleft()
+        number_of_verteces_checked += 1
+        if vertex == goal:
+            print 'found solution for BFS! number of verteces checked: ' + str(number_of_verteces_checked)
+            return True
+        neighbors = getNeighbors(vertex, board_size)
+        for n in neighbors:
+            queue.append(n)
 
 def AStar(board):
     print 'AStar'
@@ -95,7 +127,7 @@ def main():
         IDDFS(initial_position, goal_state, board_size, board_size)
         #IDS(initial_position, goal_state, board_size)
     elif lines[0] == '2':
-        BFS(initial_position)
+        BFS(board_size, initial_position, goal_state)
     elif lines[0] == '3':
         AStar(initial_position)
     else:
