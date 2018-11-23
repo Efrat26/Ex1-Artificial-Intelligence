686bb03aeda332b481716f2f33af62878b8807ef
efiso <efisofer26@gmail.com>
2018-11-23 19:18:19 +0200

    backup commit added heuristic function calculation and priority queue checked the BFS and IDS on some inputs

--- venv/main.py
+++ venv/main.py
@@ -1,6 +1,8 @@
 import sys
 import collections
 #Efrat Sofer 304855125
+
+####### node class ##########
 class Node:
     def __init__(self):
         visited = 0
@@ -13,6 +15,46 @@ class Node:
     def setParent(self, p):
         self.parent = p
 
+####### pririty queue for A* ##########
+'''
+A simple implementation of Priority Queue using Queue.
+i used the code from here:
+https://www.geeksforgeeks.org/priority-queue-in-python/
+'''
+class PriorityQueue(object):
+    def __init__(self):
+        self.queue = []
+
+    def __str__(self):
+        return ' '.join([str(i) for i in self.queue])
+
+        # for checking if the queue is empty
+
+    def isEmpty(self):
+        return len(self.queue) == []
+
+        # for inserting an element in the queue
+
+    def insert(self, data):
+        self.queue.append(data)
+
+        # for popping an element based on Priority
+
+    def delete(self):
+        try:
+            max = 0
+            for i in range(len(self.queue)):
+                if self.queue[i] > self.queue[max]:
+                    max = i
+            item = self.queue[max]
+            del self.queue[max]
+            return item
+        except IndexError:
+            print()
+            exit()
+
+
+####### general methods ##########
 def getGoalState(board_size):
     result = ''
     for i in range(1,board_size*board_size+1):
@@ -80,15 +122,17 @@ def getPath(solution_list):
             path.append('D')
     return path
 
-def DLS(src, target, limit, board_size, solution_list, vertex_counter):
+
+#######IDS##########
+def DLS(src, target, limit, board_size, solution_list, vertex_counter, solution):
     if src == target:
         return True
     if limit <= 0:
         return False
     neighbors = getNeighbors(src, board_size)
     for n in neighbors:
-        vertex_counter += 1
-        if DLS(n, target, limit - 1, board_size, solution_list):
+        vertex_counter[0] = vertex_counter[0] + 1
+        if DLS(n, target, limit - 1, board_size, solution_list, vertex_counter,  solution):
             solution_list.append(n)
             return True
     return False
@@ -97,13 +141,17 @@ def DLS(src, target, limit, board_size, solution_list, vertex_counter):
 def IDDFS(src, target, max_depth, board_size):
     for limit in range(0, max_depth):
         solution = [src]
-        vertex_counter = 0
-        print 'begin depth: ' + str(limit)
-        if DLS(src, target, limit, board_size, solution, vertex_counter) == True:
+        vertex_num = []
+        vertex_num.append(1)
+        #vertex_counter = 0
+        print ('begin depth: ' + str(limit))
+        if DLS(src, target, limit, board_size, solution, vertex_num, solution):
             path = getPath(solution)
-            return [path, vertex_counter, limit]
+            return [path, vertex_num[0], limit]
     return []
 
+
+#######BFS##########
 def getPathFromLastNodeBfs(last_node):
     result = [last_node.state]
     stop = False
@@ -132,7 +180,7 @@ def BFS(board_size, root, goal):
         #vertex_node.setState(vertex)
         number_of_verteces_checked += 1
         if vertex.state == goal:
-            print 'found solution for BFS! number of verteces checked: ' + str(number_of_verteces_checked)
+            print ('found solution for BFS! number of verteces checked: ' + str(number_of_verteces_checked))
             path_nodes = getPathFromLastNodeBfs(vertex)
             path = getPath(path_nodes)
             return [path, number_of_verteces_checked, 0]
@@ -143,13 +191,41 @@ def BFS(board_size, root, goal):
             n_node.setParent(vertex)
             queue.append(n_node)
 
-def AStar(board):
-    print 'AStar'
+
+####### A* ##########
+def getRowAndColIndInMatrix(state, element):
+    splitted_state = state.split('-')
+    board_size = len(splitted_state)
+    board_size_sqrt = board_size ** (1/2.0)
+    index_of_element = splitted_state.index(element)
+    col_index = index_of_element%board_size_sqrt
+    row_index = 0
+    counter_top = -1
+    for i in range(0, board_size):
+        counter_top += board_size_sqrt
+        if index_of_element <= counter_top:
+            row_index = i
+            break
+    return [row_index, int(col_index)]
 
 
+
+def calculateHeuristic(state, goal, element):
+    [row_s, col_s] = getRowAndColIndInMatrix(state, element)
+    [row_g, col_g] = getRowAndColIndInMatrix(goal, element)
+    result = abs(row_s - row_g) + abs(col_s - col_g)
+    return result
+
+def AStar(intial, goal):
+    calculateHeuristic(intial, goal)
+    print ('AStar')
+
+
+
+####### main function ##########
 def main():
     if len(sys.argv) != 2:
-        print 'the program needs one input argument'
+        print ('the program needs one input argument')
         return
     input_file_name = sys.argv[1]
     f_input = open(input_file_name, "r")
@@ -157,7 +233,7 @@ def main():
     try:
         board_size = int(lines[1])
     except ValueError:
-        print 'board size is not a valid integer'
+        print ('board size is not a valid integer')
         return
     initial_position = lines[2]
     goal_state = getGoalState(board_size)
@@ -168,16 +244,16 @@ def main():
     elif lines[0] == '2':
         solution = BFS(board_size, initial_position, goal_state)
     elif lines[0] == '3':
-        solution = AStar(initial_position)
+        solution = AStar(initial_position, goal_state)
     else:
-        print 'algorithm number should be integer between 1 to 3'
+        print ('algorithm number should be integer between 1 to 3')
         return
     if len(solution) == 3:
         path = solution[0]
         string_path = ''
         for i in range(0, len(path)):
             string_path += path[i]
-        print string_path + ' ' + str(solution[1]) + ' ' + str(solution[2])
+        print (string_path + ' ' + str(solution[1]) + ' ' + str(solution[2]))
 
 
 
